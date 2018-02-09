66733c2f4d0c552e6cfda2fa00708eca90455221
Biagini <dabi@MobicaPL.local>
2018-02-09 15:58:14 +0000

    Added Index.html handlight, images dont work

--- flowerServer.js
+++ flowerServer.js
@@ -1,5 +1,7 @@
 // Load the http module
 var http = require('http');
+var fs = require('fs'); //require filesystem module
+
 
 var PythonShell = require('python-shell');
 var options = {
@@ -13,10 +15,15 @@ var options = {
 var output= '';
 var server;
 var html;
-setInterval(checkStatusInterval, 20000);//20Secs
+var userCount=0;
+var temp=0;
+var fertility=0;
+var sunlight=0;
+var moisture=0;
+setInterval(checkStatusInterval, 10000);//20Secs
 
-setTimeout(startHTMLServer, 10000);
-
+//setTimeout(startHTMLServer, 10000);
+startHTMLServer();
 function buildHtml(request){
 	var header = "";
 	var body = output;
@@ -26,29 +33,41 @@ function buildHtml(request){
 }
 
 function checkStatusInterval(){
-	PythonShell.run('demo.py', options, function (err,output){
+	/*PythonShell.run('demo.py', options, function (err,output){
 		if (err) throw err;
 		///collect results
 		console.log('output: %j', output);
-	});
+	});*/
+	output="Temperature="+temp.toString()+" Moisture="+moisture.toString()+" Sunlight= "+sunlight.toString()+" Fertility="+fertility.toString();
+	temp++;
+	fertility++;
+	sunlight++;
+	moisture++;
 
 }
 
 function startHTMLServer(){
 //Configure http server to respond
-server = http.createServer(function (request, response) {
-	html = buildHtml(request);
-	response.writeHead(200, {
-		"Content-Type": "text/html",
-		"Content-Length": html.length,
-		"Expires": new Date().toUTCString()
-	});
-response.end(html);
-});
+
+server = http.createServer(httpHandler)
+
 //Listen on port
 server.listen(8000);
 //Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1:8000/");
 }
 
+function httpHandler (request, response) { //create server
+  fs.readFile('./index.html', function(err, data) { //read file index.html in public folder
+    if (err) {
+      response.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
+      return response.end("404 Not Found");
+    }
+    response.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
+    response.write(data); //write data from index.html
+    response.write("We have "+userCount+" visits \n");
+    response.write(" "+output+" \n");
+    return response.end();
+  });
+}
 
