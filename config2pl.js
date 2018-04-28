00e0adb29b165030b8ad14269ce700ceba18aa69
dabi <davidbiagini.pro@gmail.com>
2018-04-28 12:15:22 +0200

    few bug fixes and double plant config

--- config2pl.js
+++ config2pl.js
@@ -8,14 +8,17 @@ plant0 = {
 	temperature : [0,10,32], // |actual|min|max|
 	fertility : [0,350,2000],
 	sunlight : [0,2000,4000],
-	moisture : [0,10,50],//used as irrigation limit
+	moisture : [0,15,40],//used as irrigation limit
 	battery : [0,100],
 	lastWarning : "",
 	mac : 'C4:7C:8D:65:F8:FB', //mac address
-	settling : true, // |true|false
-	gpio : 17, //gpio number for pump
+	settling : false, // |true|false
+	gpio : 14, //gpio number for pump
 	pump: null, //placeholder for gpio
-	unit : 2000 //ms in order to deliver 25cl
+	unit : 20000, //ms in order to deliver 25cl
+	maxUnits : 7, //max value to irrigate
+	refuelCounter : 0 //counter used to check max refuel
+
 };
 
 plant1 = {
@@ -23,14 +26,17 @@ plant1 = {
 	temperature : [0,10,32], // |actual|min|max|
 	fertility : [0,350,2000],
 	sunlight : [0,2000,4000],
-	moisture : [0,10,50], ///used as irrigation limit
+	moisture : [0,15,40], ///used as irrigation limit
 	battery : [0,100],
 	lastWarning : "",
 	mac : 'C4:7C:8D:65:FB:29', //mac address
-	settling : true, // |true|false
-	gpio : 18, //gpio number for pump
+	settling : false, // |true|false
+	gpio : 15, //gpio number for pump
 	pump: null, //placeholder for GPIO
-	unit : 2000 //ms in order to deliver 25cl
+	unit : 20000, //ms in order to deliver 25cl
+	maxUnits : 7, //max value to irrigate
+	refuelCounter : 0 //counter used to check max refuel
+
 };
 
 config.plants.push(plant0);
