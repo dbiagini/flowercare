75634ed6fe0723655299781873c3326dfad7d75e
Biagini, David <david.biagini@mobica.com>
2019-06-21 16:08:27 +0100

    adding plant class to be expanded

--- flowerServer.js
+++ flowerServer.js
@@ -8,6 +8,7 @@ var gpio = null;
 var srv = require('./htmlServer');
 var Pump = require('./pump.js');
 var LED = require('./led.js');
+var Plant = require('./plant.js');
 console.log(" platform %s \n", process.platform);
 if (process.platform != "win32") gpio = require('onoff').Gpio;
 const server = new srv();
@@ -47,31 +48,33 @@ if (process.platform != "win32") {
 	statusLED = new LED(4);//new gpio(4, 'out'); ///first pin as status led
 	statusLED.Blink(); //starting up
 }
+
+///setup garden
+var garden = [];
+for (i = 0; i< config.plants.length; i++){
+	garden.push(new Plant(config.plants[i]));
+}
 if(config.useSim) {
 	console.log('Running Sensor simulation \n');
-	for (i = 0; i< config.plants.length; i++){
-		config.plants[i].temperature[0] = config.plants[i].temperature[1];
-		config.plants[i].fertility[0] = config.plants[i].fertility[1];
-		config.plants[i].sunlight[0] = config.plants[i].sunlight[1];
-		config.plants[i].moisture[0] = config.plants[i].moisture[1]+20;
-		config.plants[i].battery[0] = config.plants[i].battery[1];
+	for (i = 0; i< garden.length; i++){
+		garden[i].initSimulation();
 	}
 
 }
 for (var i = 0; i< config.plants.length; i++){
 	//initialize GPIO
 	if(config.irrigate && (process.platform != "win32")){
-		config.plants[i].pump = new Pump(config.plants[i].gpio);//new gpio(config.plants[i].gpio, 'out');
+		garden[i].initPump();
 		//config.plants[i].pump.writeSync(0); ///initialize the pump to 0
 	}
-	checkStatusInterval(config.plants[i]);
+	checkStatusInterval(garden[i]);
 	if(!config.useSim){ 
-		setInterval(checkStatusInterval, config.interval, config.plants[i]);//every half an hour(1.8M) update
+		setInterval(checkStatusInterval, config.interval, garden[i]);//every half an hour(1.8M) update
 	} else { 
-		setInterval(checkStatusInterval, config.intervalSim, config.plants[i]);//every 10 minutes an hour update
+		setInterval(checkStatusInterval, config.intervalSim, garden[i]);//every 10 minutes an hour update
 		console.log('been here');
 	}
-	console.log('config: %j', config.plants[i]);
+	console.log('config: %j', garden[i]); //log plant status
 }
 
 
@@ -92,14 +95,22 @@ function checkStatusInterval(plant){
 		});
 
 	} else {
-	//output="Temperature="+temperature.toString()+" Moisture="+moisture.toString()+" Sunlight= "+sunlight.toString()+" Fertility="+fertility.toString();
-		//plant.temperature[0] -= Math.floor(Math.random() * 5);
-		//plant.fertility[0] -= Math.floor(Math.random() * 5);
-		//plant.sunlight[0] -= Math.floor(Math.random() * 5);
-		plant.moisture[0] -= Math.floor(Math.random() * 10);
-		//plant.battery[0]  -= Math.floor(Math.random()* 5);
-		console.log('plant: %s, sim moisture %d ', plant.name, plant.moisture[0]);
-		checkIrrigate(plant);
+
+		//this runs only if the sensor simulation is being used
+		//output="Temperature="+temperature.toString()+" Moisture="+moisture.toString()+" Sunlight= "+sunlight.toString()+" Fertility="+fertility.toString();
+			//plant.temperature[0] -= Math.floor(Math.random() * 5);
+			//plant.fertility[0] -= Math.floor(Math.random() * 5);
+			//plant.sunlight[0] -= Math.floor(Math.random() * 5);
+			plant.moisture[0] -= Math.floor(Math.random() * 10);
+			//plant.battery[0]  -= Math.floor(Math.random()* 5);
+			console.log('plant: %s, sim moisture %d ', plant.name, plant.moisture[0]);
+		if(config.irrigate && (!sensorStartingUp)) {
+			plant.checkIrrigate();
+		} else {
+			//debug string//
+			if(config.debug) console.log('plant: %s status, moisture %d settling %d sensorStarted %d refuelCounter %d irrigate %b', plant.name, plant.moisture[0], plant.settling, sensorStartingUp, plant.refuelCounter, config.irrigate);
+		}
+
 	}
 
 }
@@ -110,82 +121,27 @@ function handlePostcheck(err, output, plant){
 		//handle errors from Python
 		//Don't change Values
 		//should print a warning in the page
-		d = new Date();
-		plant.lastWarning = d.toUTCString() + "Issue with connection to the sensor";
+		plant.updateLastWarning("Issue with connection to the sensor");
 	} else {
-			plant.temperature[0] = output[0];
-			plant.moisture[0] = output[1];
-			plant.sunlight[0] = output[2];
-			plant.fertility[0] = output[3];
-			plant.battery[0] = output[4];
+		plant.updateStatus(output);		
 	}
 	//log in any case
 	console.log('output: %j', output);
 
-	checkIrrigate(plant); //values should be updated by now.
+	if(config.irrigate && (!sensorStartingUp)) {
+	
+		plant.checkIrrigate();
 
-}
-
-function checkIrrigate(plant){
-///after updating the status compare the water level and kick the refueling // plant settling is set true during the first irrigation and turned of when the 
-	if(config.irrigate && (!sensorStartingUp)){	
-		if ((((plant.moisture[0] <= plant.moisture[1]) && (!plant.settling)) || ((plant.moisture[0] > plant.moisture[1]) && (plant.moisture[0] < plant.moisture[2]) && (plant.settling))) && (plant.refuelCounter <= plant.maxUnits)){
-
-				console.log('plant: %s needs refueling, moisture %d settling %d ', plant.name, plant.moisture[0], plant.settling);
-				refuelPlant(plant);
-		} else if ((plant.moisture[0] >= plant.moisture[2]) && (plant.settling)) {
-			plant.settling = false; //finished refueling.
-			plant.refuelCounter = 0;//reset limit counter
-			console.log('plant: %s finished refueling, moisture %d settling %d ', plant.name, plant.moisture[0], plant.settling);
-		} else if ((plant.moisture[0] <= plant.moisture[1]) && (plant.settling)){
-		  ///something is wrong the refueling happened and it's not having effects
-			d = new Date();
-			plant.lastWarning = d.toUTCString() + " ERROR refueling failed or Sensor not responding";
-			console.log('ERROR: plant: %s, moisture %d refueling or Sensor not working!!!', plant.name, plant.moisture[0]);
-
-		} else if (plant.refuelCounter > plant.maxUnits){
-			d = new Date();
-			plant.lastWarning = d.toUTCString() + " ERROR irrigation units reached the limit";
-			plant.settling = false; //let the plant settle.
-			console.log('ERROR: plant: %s, moisture %d irrigation units reached the limit # %d !!!', plant.name, plant.moisture[0], plant.refuelCounter);
-		} else {
-		//debug string//
-		if(config.debug) console.log('plant: %s no irrigation, moisture %d settling %d sensorStarted %d refuelCounter %d irrigate %b', plant.name, plant.moisture[0], plant.settling, sensorStartingUp, plant.refuelCounter, config.irrigate);
-				
-		}
 	} else {
 		//debug string//
-		if(config.debug) console.log('plant: %s status, moisture %d settling %d sensorStarted %d refuelCounter %d irrigate %b', plant.name, plant.moisture[0], plant.settling, sensorStartingUp, plant.refuelCounter, config.irrigate);
-	}
-}
-
-
-
-function refuelPlant(plant){
-
-	///
-	plant.settling = true;
-	diff = plant.moisture[2] - plant.moisture[0];
-	units = Math.abs(Math.ceil(diff/20));//one unit is 25cl, increase ~20%  assuming linear model, simplistic
-	if(units >= plant.maxUnits) units=plant.maxUnits; //limitate max irrigation
-	plant.refuelCounter+= units;//increment limit counter
-	console.log("refueling %d units \n", units);
-	///irrigate n units
-	if(plant.pump){
-
-		/*if(!config.useSim)*/ plant.pump.On(); //turn on plant
-		/*if(!config.useSim)*/ setTimeout(plant.pump.Off, (plant.unit * units));//turn it off later
-		if(config.useSim) {
-			plant.moisture[0] +=(20 * units);
+		if(config.debug) { 
+			
+			plant.logStatus();
+			console.log(' sensorStarted %d irrigate %b', sensorStartingUp, config.irrigate);
 		}
 	}
 
-	//console.log("plant new moisture %d \n", plant.moisture[0]);
-}
 
-function settled(plant){
-	console.log("ground should be settled! \n");
-	plant.settling = false; ///allow irrigating
 }
 
 
---
+++ plant.js
@@ -0,0 +1,162 @@
+module.exports = class Plant {
+
+	/*constructor() {
+		//initialize plant internal variables
+		this.name = "";
+		this.temperature = [0,0,0];// vector |actual|min|max|
+		this.fertility = [0,0,0]; //vector |actual|min|max|
+		this.sunlight = [0,0,0]; //vectpr |actual|min|max|
+		this.moisture = 0; ///used as irrigation limit
+		this.battery = 0; //battery  status
+		this.lastWarning = ''; //sensor issues log
+		this.mac = 0; //mac address
+		this.settling = false; // |true|false
+		this.gpio = 0; //gpio pin number for pump
+		this.pump = null; //placeholder for GPIO instance
+		this.unit = 0; //ms pump opening in order to deliver 25cl
+		this.maxUnits = 0; //max value to irrigate
+		this.refuelCounter = 0 //counter used to check max refuel
+	}*/
+	constructor(plant) {
+		//initialize plant internal variables
+		this.name = plant.name;
+		this.temperature = plant.temperature;// vector |actual|min|max|
+		this.fertility = plant.fertility; //vector |actual|min|max|
+		this.sunlight = plant.sunlight; //vectpr |actual|min|max|
+		this.moisture = plant.moisture; ///used as irrigation limit
+		this.battery = plant.battery; //battery  status
+		this.lastWarning = ''; //sensor issues log
+		this.mac = plant.mac; //mac address
+		this.settling = false; // |true|false
+		this.gpio = plant.gpio; //gpio pin number for pump
+		this.pump = null; //placeholder for GPIO instance
+		this.unit = plant.unit; //ms pump opening in order to deliver 25cl
+		this.maxUnits = plant.maxUnits; //max value to irrigate
+		this.refuelCounter = 0 //counter used to check max refuel
+	}
+
+	initPump(){
+	
+		if(this.gpio != 0){
+			this.pump = new Pump(this.gpio);
+		}
+	}
+
+	initPlant(plant){
+		//initialize plant internal variables
+		if(plant.name) this.name = plant.name;
+		if(plant.temperature) this.temperature = plant.temperature;// vector |actual|min|max|
+		if(plant.fertility) this.fertility = plant.fertility; //vector |actual|min|max|
+		if(plant.sunlight) this.sunlight = plant.sunlight; //vectpr |actual|min|max|
+		if(plant.moisture) this.moisture = plant.moisture; ///used as irrigation limit
+		if(plant.battery) this.battery = plant.battery; //battery  status
+		this.lastWarning = ''; //sensor issues log
+		if(plant.mac) this.mac = plant.mac; //mac address
+		this.settling = false; // |true|false
+		this.pump = null; //placeholder for GPIO instance
+		if(plant.unit) this.unit = plant.unit; //ms pump opening in order to deliver 25cl
+		if(plant.maxUnits) this.maxUnits = plant.maxUnits; //max value to irrigate
+		this.refuelCounter = 0 //counter used to check max refuel
+
+	
+		if(plant.gpio != 0){
+			this.gpio = plant.gpio;
+			this.pump = new Pump(plant.gpio);
+		}
+	}
+
+	initSimulation(){
+		this.temperature[0] = this.temperature[1];
+		this.fertility[0] = this.fertility[1];
+		this.sunlight[0] = this.sunlight[1];
+		this.moisture[0] = this.moisture[1]+20;
+		this.battery[0] = this.battery[1];
+	
+	}
+	refuelPlant(){
+
+		///
+		this.settling = true;
+		var diff = this.moisture[2] - this.moisture[0];
+		var units = Math.abs(Math.ceil(diff/20));//one unit is 25cl, increase ~20%  assuming linear model, simplistic
+		if(units >= this.maxUnits) units=this.maxUnits; //limitate max irrigation
+		this.refuelCounter+= units;//increment limit counter
+		console.log("refueling %d units \n", units);
+		///irrigate n units
+		if(this.pump){
+
+			/*if(!config.useSim)*/ this.pump.On(); //turn on plant
+			/*if(!config.useSim)*/ setTimeout(this.pump.Off, (this.unit * units));//turn it off later
+			if(config.useSim) {
+				this.moisture[0] +=(20 * units);
+			}
+		}
+
+		//console.log("plant new moisture %d \n", plant.moisture[0]);
+	}
+
+	settled(){
+		console.log("ground should be settled! \n");
+		this.settling = false; ///allow irrigating
+	}
+
+	checkIrrigate(){
+	///after updating the status compare the water level and kick the refueling // plant settling is set true during the first irrigation and turned of when the 
+		if ((((this.moisture[0] <= this.moisture[1]) && (!this.settling)) || ((this.moisture[0] > this.moisture[1]) && (this.moisture[0] < this.moisture[2]) && (this.settling))) && (this.refuelCounter <= this.maxUnits)){
+
+			console.log('plant: %s needs refueling, moisture %d settling %d ', this.name, this.moisture[0], this.settling);
+			refuelPlant();
+		} else if ((this.moisture[0] >= this.moisture[2]) && (this.settling)) {
+			this.settling = false; //finished refueling.
+			this.refuelCounter = 0;//reset limit counter
+			console.log('plant: %s finished refueling, moisture %d settling %d ', this.name, this.moisture[0], this.settling);
+		} else if ((this.moisture[0] <= this.moisture[1]) && (this.settling)){
+		  ///something is wrong the refueling happened and it's not having effects
+			
+			this.updateLastWarning(" ERROR refueling failed or Sensor not responding");
+			//var d = new Date();
+			//this.lastWarning = d.toUTCString() + " ERROR refueling failed or Sensor not responding";
+			console.log('ERROR: plant: %s, moisture %d refueling or Sensor not working!!!', this.name, this.moisture[0]);
+
+		} else if (this.refuelCounter > this.maxUnits){
+			
+			this.updateLastWarning(" ERROR irrigation units reached the limit");
+
+			//var d = new Date();
+			//plant.lastWarning = d.toUTCString() + " ERROR irrigation units reached the limit";
+			this.settling = false; //let the plant settle.
+			console.log('ERROR: plant: %s, moisture %d irrigation units reached the limit # %d !!!', this.name, this.moisture[0], this.refuelCounter);
+		} else {
+		//debug string//
+			this.logStatus();
+				
+		}
+		
+	}
+
+	updateLastWarning(err){
+		var d = new Date();
+		this.lastWarning = d.toUTCString() + err;
+
+	}
+
+	updateStatus(input){
+	
+		if(input){
+			this.temperature[0] = input[0];
+			this.moisture[0] = input[1];
+			this.sunlight[0] = input[2];
+			this.fertility[0] = input[3];
+			this.battery[0] = input[4];
+
+		} else {
+		   console.log("empty input vector UpdateStatus");
+		}
+	}
+
+	logStatus(){
+		console.log('plant: %s status, moisture %d settling %d refuelCounter %d ', this.name, this.moisture[0], this.settling, this.refuelCounter);
+	}
+
+
+}
